921b4e09715a05b5942728da5e4fcdc83b536959
art1shok <kvitkaart554@gmail.com>
2019-12-09 10:34:53 +0200

    Updates

---
+++ precache-manifest.acd3d8a6915d93bee7b59452a38631fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340d8c326ae23043506f876c0e1e5da9",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "bc7f18ee6da6aff47f01",
+    "url": "/react_tabs/static/css/main.124106d5.chunk.css"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/react_tabs/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "bc7f18ee6da6aff47f01",
+    "url": "/react_tabs/static/js/main.46e6bf1d.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
